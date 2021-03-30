3083164215a0e5f30fd77f5ab06d35150d32a0c4
Nicolas Pietro <niceniick@gmail.com>
2021-03-30 14:24:38 +0000

    Update labirinth.py

--- labirinth.py
+++ labirinth.py
@@ -4,21 +4,22 @@ def findPath(board,path,i,j,n):
         return 1
     
     if board[i][j] == 1:
-        path[i][j] = 1
-
         if findPath(board,path,i,j+1,len(board)) == 1:
+            path[i][j] = 1
             return 1
+            
         if findPath(board,path,i+1,j,len(board)) == 1:
-            return 1
-        
-        path[i][j] = 0
+            path[i][j] = 1
+            return 1  
+
+    path[i][j] = 0
     
     return 0
 
 board = [
     [1,1,1,0],
-    [0,0,1,0],
-    [0,1,1,0],
+    [1,0,0,0],
+    [1,0,0,0],
     [1,1,1,1],
 ]
 
@@ -38,10 +39,11 @@ for i in range(len(board)):
     for j in range(len(board)):
         if path[i][j]:
             count+=1
-            if path[i][j] > path[i][j-1]:
-                print('[x]', end='')
-            else:
-                print('[x]', end='')
+            print('[x]', end='')
+            # if path[i][j] > path[i][j-1]:
+            #     print('[x]', end='')
+            # else:
+            #     print('[x]', end='')
         else:
             print('[ ]', end='')
     print()
